087198233761157ffc3aa98bc98ecce3074b231c
Anastasya <gbshell2022@gmail.com>
2024-10-19 10:43:38 +0300

    maven

--- src/main/java/ru/geekbrain/App.java
+++ src/main/java/ru/geekbrain/App.java
@@ -4,10 +4,15 @@ package ru.geekbrain;
  * Hello world!
  *
  */
+import  com.google.common.base.Joiner;
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        String[] words = {"Hello", "World"};
+        String message= Joiner.on (" , ").join(words);
+
+        System.out.println(message);
     }
 }
